15d9c1197a684edf59f2a5ed22676546270e5517
songkovong <60258509+songkovong@users.noreply.github.com>
2025-08-20 20:30:08 +0900

    #57 조금 추가
    - 퀵 포션 추가
    - Swarm, Wing Swarm 몬스터 추가
    - Blue sword 추가
    - 일본풍 맵 살짝? 애매하긴함

---
+++ Assets/Scripts/Enemy/Enemies/Swarm.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarm : Enemy, IDamageable
+{
+    [SerializeField] private List<GameObject> dropItems;
+    [SerializeField] private GameObject dropCoin;
+    protected override void Awake()
+    {
+        base.Awake();
+        thisEnemyCode = 7;
+        attackCooltime = 0.9f;
+        detectRadius = 10f;
+        detectAttackRadius = 1.5f;
+        moveSpeed = 1.8f;
+        rotationSpeed = 2000f;
+        attackDamage = 13f;
+        maxHealth = 32f;
+        currentHealth = maxHealth;
+        godmodeDuration = 0.6f;
+        dependRate = 0.3f;
+    }
+
+    public override void TakeDamage(int minDamage, int maxDamage)
+    {
+        base.TakeDamage(minDamage, maxDamage);
+        Debug.Log("Swarm Damaged");
+    }
+
+    public override void ExpUp()
+    {
+        base.ExpUp();
+        playerstat.data.expCount += 20;
+    }
+
+    public override void EnemyDie() // In DeadState
+    {
+        if (spawner != null)
+        {
+            spawner.NotifyEnemyDead(gameObject);
+        }
+        else
+        {
+            DestroyEnemy();
+        }
+    }
+
+    public override void DropItem()
+    {
+        foreach (GameObject dropItem in dropItems)
+        {
+            float rate = Random.Range(0f, 1f);
+            // Item Drop
+            if (dropItem?.GetComponent<ItemPickUp>().item.itemDropRate >= rate)
+            {
+                var rotation = Random.Range(0f, 180f);
+                GameObject dropped = Instantiate(dropItem, transform.position, Quaternion.Euler(rotation, 0f, rotation));
+                Rigidbody rb = dropped.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    var randomUp = 0.05f;
+                    var randomRight = Random.Range(-0.05f, 0.05f);
+                    rb.AddForce(Vector3.up * randomUp + Vector3.right * randomRight, ForceMode.Impulse);
+                }
+                Debug.Log("Item Dropped");
+            }
+        }
+    }
+
+    public override void DropCoin()
+    {
+        float rate = Random.Range(0f, 1f);
+        if (dropCoin?.GetComponent<CoinPickUp>().coin.coinDropRate >= rate)
+        {
+            var rotation = Random.Range(0f, 180f);
+            GameObject coindropped = Instantiate(dropCoin, transform.position, Quaternion.Euler(rotation, 0f, rotation));
+            Rigidbody rb = coindropped.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                var randomUp = 0.05f;
+                var randomRight = Random.Range(-0.05f, 0.05f);
+                rb.AddForce(Vector3.up * randomUp + Vector3.right * randomRight, ForceMode.Impulse);
+            }
+            Debug.Log("Coin Dropped");
+        }
+    }
+}
